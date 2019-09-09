9ba054880d8428d40ee575cdbed93c69895f27c2
Wyatt King <wkmusic8@gmail.com>
2019-09-09 13:47:51 -0600

    server files  and nodemon set up

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const { PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const ctrl = require('./controller');
+const authCtrl = require('./authController');
+
+const app = express();
+
+app.use(express.json());
+
+// Sessions
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 10
+    }
+}));
+
+// Endpoints
+
+
+//  Listener and DB hookup
+massive(CONNECTION_STRING)
+    .then(db => {
+        app.set('db', db)
+        app.listen(PORT, () => console.log(`Motha truckin' on port ${PORT}`))
+    })
