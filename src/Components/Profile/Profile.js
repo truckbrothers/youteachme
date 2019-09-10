b0c90274004c145d48439881ab5b9425a3a772d3
ColetonS <coleton.m.smith@gmail.com>
2019-09-10 14:58:23 -0600

    set up enzyme and created toggle test for Feed

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Profile extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
