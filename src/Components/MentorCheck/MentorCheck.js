36a1aadcea1c48f80d338680defa0d37bab86518
Wyatt King <wkmusic8@gmail.com>
2019-09-10 16:38:19 -0600

    initial style for mentorcheck

--- src/Components/MentorCheck/MentorCheck.js
+++ src/Components/MentorCheck/MentorCheck.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './mentorcheck.css';
+import { Button } from 'antd';
 
 export default class MentorCheck extends Component {
     state = {
@@ -8,13 +9,21 @@ export default class MentorCheck extends Component {
     render() {
         return (
             <div className='wrapper'>
-                <div className='container'>Would you like to<br></br>
-                    <button>Mentor</button><br></br>
-                    Or<br></br>
-                    <button>Start Your Learning</button>
+                <div className='container'>Would You Like To:</div>
+                <div className='button-wrapper'>
+                    <Button
+                        type="primary"
+                        className='mentor-check-btn grow'>
+                        Mentor
+                    </Button>
+                    <Button
+                        type="primary"
+                        className='mentor-check-btn grow'>
+                        Learn
+                    </Button>
                 </div>
             </div>
         )
-        
+
     }
 };
