b59d6947cd236cd88176b3d2ad3dfea4afed1422
ColetonS <coleton.m.smith@gmail.com>
2019-09-11 11:20:26 -0600

    package-lock.json and Landing.test.js

---
+++ src/Components/Landing/Landing.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Landing from './Landing'
+import { mount, shallow } from 'enzyme'
+
+describe('testing input fields', () => {
+    it('should receive input', () => {
+        const wrapper = shallow(<Landing />)
+        const instance = wrapper.instance()
+        expect
+    })
+})
