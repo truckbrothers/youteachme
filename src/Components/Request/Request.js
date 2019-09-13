1a02be9f332718d620afb2178e8c12b2b48f1825
Wyatt King <wkmusic8@gmail.com>
2019-09-13 09:01:21 -0600

    request changes

--- src/Components/Request/Request.js
+++ src/Components/Request/Request.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './request.css'
 
 export default class Request extends Component {
     state = {
@@ -6,8 +7,14 @@ export default class Request extends Component {
     }
     render() {
         return (
-            <div className='request'>
-                
+            <div className='request-wrapper'>
+                <div className='request-container'>
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni suscipit ipsum dolores autem omnis id quisquam illum unde commodi voluptas! Est dignissimos odio accusamus ipsum, perferendis dolor quibusdam illum autem!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error rerum placeat optio reiciendis nostrum sit, quaerat repellendus, omnis incidunt cumque saepe reprehenderit ab alias modi accusamus consectetur ad voluptatem dolorum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, perferendis? Minima soluta rerum, totam maiores repellendus voluptatibus excepturi dolorum adipisci tenetur provident eos eveniet tempore voluptates sapiente officia eligendi culpa.
+                    </p>
+                </div>
             </div>
         )
     }
