933351946d459648811fb3324e01e99b77526cf8
david collett <davida.collett95@gmail.com>
2019-09-16 09:30:19 -0600

    made some changes to mentor

--- src/Components/Mentor/Mentor.js
+++ src/Components/Mentor/Mentor.js
@@ -4,17 +4,28 @@ import "./Mentor.css";
 import { connect } from 'react-redux'
 import { mentorToggle } from '../../ducks/reducer'
 import axios from 'axios'
+import Request from '../Request/Request'
 
 class Mentor extends Component {
     constructor() {
         super();
 
         this.state = {
-
+            requests: []
         };
     }
-
+    getRequests = () => {
+        axios.get(`/request/${this.props.user_id}`)
+            .then(request => this.setState({requests: request.data}))
+            .catch(err => console.log(`couldn't get requests`))
+    }
+    componentDidMount() {
+        this.getRequests()
+    }
     render() {
+        const requestsMap = this.state.requests.map((el => (
+            <Request/>
+        )))
         return (
             <div className='mentor-section'>
                 <div>
@@ -26,14 +37,15 @@ class Mentor extends Component {
                     <span className='m-mentor'>Mentor</span>
                 </div>
                 <h1>Mentor Mode</h1>
+                {requestsMap}
             </div>
         );
     }
 }
 
 function mapStateToProps(reduxState) {
-    const { toggleStatus } = reduxState
-    return { toggleStatus }
+    const { toggleStatus, user_id } = reduxState
+    return { toggleStatus, user_id }
 }
 
 export default connect(mapStateToProps, { mentorToggle })(Mentor)
